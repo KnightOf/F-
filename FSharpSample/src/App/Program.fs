02a6f4c7b27137e717fc22d47bf04be89a59c259
Shen.Sean 沈子翔 TAO <Shen.Sean@inventec.com>
2021-12-28 15:32:26 +0800

    first commit

---
+++ FSharpSample/src/App/Program.fs
@@ -0,0 +1,15 @@
+﻿
+
+
+open System
+open Library
+
+[<EntryPoint>]
+let main args = 
+ printfn "Nice command-line arguments! Here's what System.Text.Json has to say about them:"
+
+ let value,json = getJson{| args=args ; year = System.DateTime.Now.Year|}
+ printfn $"Input: %0A{value}"
+ printfn $"Output: %s{json}"
+
+ 0
